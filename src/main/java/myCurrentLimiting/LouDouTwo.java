41cc587bcfd7917d4fb2b230e8ca3b3b3cd2d8e0
wangdongdong15 <wangdongdong15@meituan.com>
2020-12-23 16:34:04 +0800

    12.23  漏洞  单机限流

---
+++ src/main/java/myCurrentLimiting/LouDouTwo.java
@@ -0,0 +1,46 @@
+package myCurrentLimiting;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author: WangDongDong
+ * @version: v1.0
+ * @create: 2020-12-23 16:20
+ * @description
+ **/
+public class LouDouTwo {
+	public static void main(String[] args) {
+		//线程池
+		ExecutorService exec = Executors.newCachedThreadPool();
+		//速率是每秒只有5个许可
+		final RateLimiter rateLimiter = RateLimiter.create(3.0);
+
+		for (int i = 0; i < 10; i++) {
+			final int no = i;
+			Runnable runnable = new Runnable() {
+				@Override
+				public void run() {
+					try {
+						//获取许可
+						rateLimiter.acquire();
+						System.out.println("Accessing: " + no + ",time:"
+								+ new SimpleDateFormat("yy-MM-dd HH:mm:ss").format(new Date()));
+
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+
+				}
+			};
+			//执行线程
+			exec.execute(runnable);
+		}
+		//退出线程池
+		exec.shutdown();
+	}
+}
